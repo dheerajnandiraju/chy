bd876ba2ae0fa61bd3e61d578b00f0612b45c987
VarshithKrishna14 <varshithkrishnakyatham@gmail.com>
2024-11-30 19:24:45 +0530

    dashboard sidebar

--- web/src/app/page.js
+++ web/src/app/page.js
@@ -1,9 +1,19 @@
+import { Sidebar } from "lucide-react";
 import Dashboard from "./dashboard/page";
 
 export default function Home() {
   return (
-    <div >
-      <Dashboard/>
+    <div className="flex">
+      {/* Sidebar Component */}
+      <div className="w-64 h-screen bg-white text-white">
+        <Sidebar className="m-4" />
+        {/* You can place your other sidebar items here */}
+      </div>
+
+      {/* Dashboard Component */}
+      <div className="flex-1 h-screen">
+        <Dashboard />
+      </div>
     </div>
   );
 }
