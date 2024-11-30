b471b69706719b4348153054112354265c98d090
tasklabs-dev <107795540+dheerajnandiraju@users.noreply.github.com>
2024-11-30 18:49:41 +0530

    dashboard

--- web/src/app/page.js
+++ web/src/app/page.js
@@ -1,9 +1,9 @@
+import Dashboard from "./dashboard/page";
+
 export default function Home() {
   return (
-    <div className="ml-16 md:ml-64 mt-16 p-4">
-      {/* The mt-16 ensures there’s space at the top for the navbar */}
-      <h1 className="text-2xl font-bold">Dashboard</h1>
-      <p>Welcome to your dashboard!</p>
+    <div >
+      <Dashboard/>
     </div>
   );
 }
